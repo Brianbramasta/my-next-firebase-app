f141b4915f6d77ea0d6a6fc364ad65c412a7068e
Brian Bramasta <brianaldybramasta@gmail.com>
2026-05-06 14:14:05 +0700

    update credential issuses

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsOP-Q9K8EtlcI_Bda7vOe2De3asx9nAU",
-    authDomain: "brianaldybramasta-7742f.firebaseapp.com",
-    projectId: "brianaldybramasta-7742f",
-    storageBucket: "brianaldybramasta-7742f.firebasestorage.app",
-    messagingSenderId: "239948994736",
-    appId: "1:239948994736:web:5f387bcb382d954801c375",
-    measurementId: "G-21JTLYHRT0"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
